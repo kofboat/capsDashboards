f074e3d5bc13b40ba7fedbd3dd12819eccd32a47
kofboat <kofboat53@gmail.com>
2022-06-12 23:09:17 -0400

    more changes

--- app.R
+++ app.R
@@ -159,7 +159,91 @@ ui <- dashboardPage(
                    infoBoxOutput("monthly.items.transac", width = 4),
                    
                    
-                 )  
+                 ),
+                 
+                 fluidRow(
+                   
+                   bs4Card(title = "Daily visitors",
+                           footer = NULL,
+                           status = "success",
+                           solidHeader = T,
+                           background = NULL,
+                           width = 12,
+                           height = NULL,
+                           collapsible = TRUE,
+                           collapsed = FALSE,
+                           closable = FALSE,
+                           maximizable = FALSE,
+                           icon = NULL,
+                           gradient = FALSE,
+                           boxToolSize = "sm",
+                           elevation = NULL,
+                           headerBorder = TRUE,
+                           label = NULL,
+                           dropdownMenu = NULL,
+                           sidebar = NULL,
+                           id = "plot.visitore",
+                           
+                           plotOutput("daily.visitors")
+                   )
+                 ),
+                 
+                 fluidRow(
+                   tabBox(
+                     id = "monthly.rev.data",
+                     collapsible = T,
+                     elevation = 2,
+                     title = "Revenue",
+                     selected = "category",
+                     status = "primary",
+                     solidHeader = FALSE,
+                     type = "tabs",
+                     tabPanel(
+                        title = "Product category",
+                        tableOutput("monthly.revenue.prod.cate")
+                       
+                     ),
+                     tabPanel(
+                       title = "Product sub-category",
+                       DTOutput("monthly.revenue.prod.subcate")
+                       
+                     ),
+                     tabPanel(
+                       title = "Product",
+                       DTOutput("monthly.revenue.product")
+                       
+                     )
+                     
+                   ),
+                   tabBox(
+                     id = "monthly.items.data",
+                     title = "items sold",
+                     selected = "Tab 2",
+                     status = "primary",
+                     solidHeader = FALSE,
+                     type = "tabs",
+                     tabPanel(
+                       title = "Product category",
+                       tableOutput("monthly.sales.prod.cate")
+                       
+                     ),
+                     tabPanel(
+                       title = "Product sub-category",
+                       DTOutput("monthly.sales.prod.subcate")
+                       
+                     ),
+                     tabPanel(
+                       title = "Product",
+                       DTOutput("monthly.sales.product")
+                       
+                     )
+                     
+                   
+                   )
+                  
+                 )
+                 
+               
                  
                  
                   )
@@ -174,83 +258,159 @@ ui <- dashboardPage(
        
    
 server <- function(input, output) {
-  # Business logic
+  # Business logic - calculations
   #--------
-  # Monthly overview  data output
+  # Monthly overview  data 
   #--------
+  
+  # revenue
   monthly.rev <- function(x){
     total.monthly.rev<-  msd1 %>% filter(month == x)  %>% 
       summarise(total_sales=sum(total_sale_price))
        total.monthly.rev }
   
+  # item sales
   monthly.item.sales <- function(x){
     total.sales.month <-  msd1 %>% filter(month == x)  %>% 
       summarise(total_sales=sum(qty))
         total.sales.month  }
   
+  # transactions
   monthly.transactions <- function(x){
     monthly.unique.visitors <- mtd1 %>% filter (month == x) %>%
       summarise(monthly.visitors = n_distinct(transactions)) 
        monthly.unique.visitors <- as_tibble(monthly.unique.visitors)
           monthly.unique.visitors   }
   
+  # expense
   monthly.expense <- function(x){
     monthly.expense <- med %>% filter( month == x) %>%
       summarise( expense= sum(amount))
         monthly.expense  }
   
+  # gross profit percent
   monthly.avg.gross.profit.percent<- function(x){
      gross.profit.margin <- msd1 %>% filter( month == x) %>%
       summarise(avg.gross.profit.margin = round(mean(gross_margin), 2))
         gross.profit.margin  }
   
+  #gross profit
   monthly.gross.prof <- function(x){
     
     gross.prof <- monthly.rev(x) - monthly.cost.of.sale(x) 
     round(gross.prof,2)
   }
   
-  
+  # cost of sale
   monthly.cost.of.sale <- function(x){
     cost.of.sale <- msd1 %>% filter( month == x) %>%
       summarise(cost_of_sale = sum(total_cost_price))
       cost.of.sale  }
   
-    
+  # net profit percent  
     monthly.net.prof.percent <- function(x){
       net.profit.percent <- round( ((monthly.rev(x) - (monthly.cost.of.sale(x) + monthly.expense(x)))
                      /monthly.rev(x)) * 100, 2)
                   net.profit.percent
     } 
-    
+  
+  # net profit    
   monthly.net.prof <- function(x){
     
     net.profit <- (monthly.rev(x) - (monthly.cost.of.sale(x) + monthly.expense(x)))
     round(net.profit,2)
   }
   
+  # items per transaction
   monthly.items.transaction <- function(x){
     items.transaction <- monthly.item.sales(x) /  monthly.transactions(x)
     items.transaction
   } 
   
+  #bank rec
   monthly.bank.reconiliation <- function(x){
-    
-    bank <- monthly.rev(x) - (monthly.expense(x) + monthly.inventory.purchase(x))
-    round(bank,2)
-    
-  }
+    bank <- monthly.rev(x) - (monthly.expense(x) 
+                              + monthly.inventory.purchase(x))
+    round(bank,2) }
   
-  
+  #inventory closing balance
   monthly.closing.balance <- function(x){
+    monthly.open.bal(x) + monthly.inventory.purchase(x) 
+     -  monthly.cost.of.sale(x)  }
+  
+  
+  # Monthly drill-down 
+  # revenue by product category
+  month.rev.prod.cat <- function(x){(
+    monthly.rev.prod.cat<-  msd1 %>% filter(month == x)  %>% 
+      group_by(prod_cat)%>%
+      summarise(total_sales=sum(total_sale_price))
+  )
+    monthly.rev.prod.cat <- monthly.rev.prod.cat[order(
+      -monthly.rev.prod.cat$total_sales),]
+    monthly.rev.prod.cat}
+  
+  # revenue by product sub-category
+  month.rev.sub.cat <- function(x){(
+    monthly.rev.sub.cat<-  msd1 %>% filter(month == x)  %>% 
+      group_by(sub_cat)%>%
+      summarise(total_sales=sum(total_sale_price))
+  )
+    monthly.rev.sub.cat<- monthly.rev.sub.cat[order(
+      -monthly.rev.sub.cat$total_sales),]
+    monthly.rev.sub.cat }
     
-    monthly.open.bal(x) + monthly.inventory.purchase(x) -  monthly.cost.of.sale(x)
     
-  }
-  
-  
-  
-  
+  # revenue by product 
+    month.rev.prod <- function(x){(
+      monthly.rev.prod<-  msd1 %>% filter(month == x)  %>% 
+        group_by(product,sub_cat)%>%
+        summarise(total_sales=(total_sale_price))
+    )
+      monthly.rev.prod <- monthly.rev.prod[order(
+        -monthly.rev.prod$total_sales),]
+      monthly.rev.prod }  
+    
+    
+  # items sold by product category
+    month.sales.prod.cat <- function(x){
+      monthly.prod.cat.sales <-  msd1 %>% filter(month == x)  %>% 
+        group_by(prod_cat)%>%
+        summarise(total_sales=sum(qty))
+      monthly.prod.cat.sales }
+    
+    # items sold by product sub-category
+    month.sales.sub.cat <- function(x){
+      (monthly.sales.sub.cat <-  msd1 %>% filter(month == x)  %>% 
+         group_by(sub_cat)%>%
+         summarise(total_sales=sum(qty))
+      )
+      monthly.sales.sub.cat <- monthly.sales.sub.cat[order(
+        -monthly.sales.sub.cat$total_sales),]
+      monthly.sales.sub.cat }             
+    
+    # items sold by product              
+    month.sales.prod <- function(x){
+      
+      (monthly.sales.prod <-  msd1 %>% filter(month == x)  %>% 
+         group_by(product,sub_cat)%>%
+         summarise(total_sales=(qty))
+      )
+      monthly.sales.prod<- monthly.sales.prod[order(
+        -monthly.sales.prod$total_sales),]
+      monthly.sales.prod }             
+    
+    # daily transactions
+    daily.transactions <- function(x){
+      visitors <- mtd1 %>% filter (month == x) %>%
+        count(Date) 
+      (visitors <- visitors[order(-visitors$n),])
+      
+      visitors %>% mutate(Date=fct_reorder(Date, -n)) %>%
+        ggplot +
+        geom_col(mapping = aes(Date,n)) }
+    
+    
   
   #output logic
   
@@ -312,8 +472,38 @@ server <- function(input, output) {
             color = "success")
   })
   
+  # monthly transactions drill-down
+  output$daily.visitors <- renderPlot({
+    daily.transactions(input$month)
+  })
+  
+ #monthly revenue drill-down
+  output$monthly.revenue.prod.cate <- renderTable({
+    month.rev.prod.cat(input$month) },colnames= FALSE )
+  
+  output$monthly.revenue.prod.subcate <- renderDT((
+    month.rev.sub.cat(input$month)), options = list(pageLength = 8),
+    colnames = c("Sub-Category","Total Sales")
+  )
   
   
+  output$monthly.revenue.product <- renderDT((
+    month.rev.prod(input$month)), options = list(pageLength = 8),
+    colnames = c("Product","Sub-Category","Total Sales"))
+  
+  #monthly item sales drill-down
+  output$monthly.sales.prod.cate <- renderTable((
+    month.sales.prod.cat(input$month)), colnames= FALSE  )
+  
+  output$monthly.sales.prod.subcate <- renderDT((
+    month.sales.sub.cat(input$month)), options = list(pageLength = 8),
+    colnames = c("Sub-Category","Total Sales"))
+  
+  output$monthly.sales.product <- renderDT((
+    month.sales.prod(input$month)), options = list(pageLength = 8),
+    colnames = c("Product","Sub-Category","Total Sales"))
+  
+ 
   
   }
   
